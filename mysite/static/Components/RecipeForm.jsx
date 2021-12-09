8ca5da8194da189b17aa753f9c32e3e9619799aa
Gregorio21 <GAGlvj@gmail.com>
2021-12-09 18:36:18 -0500

    Adding react

---
+++ mysite/static/Components/RecipeForm.jsx
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+const e = React.createElement
+
+class RecipeForm extends React.Component {
+    render() {
+        return (
+            context.recipe
+        );
+    }
+}
+
+const form = document.querySelector('#RecipeForm');
+ReactDOM.render(<RecipeForm  />, form);
